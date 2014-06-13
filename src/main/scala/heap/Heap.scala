daa696be6a74cc3226241a3ab590f9e7f13c77ff
Forex <forex.kaiz@gmail.com>
2014-06-13 10:41:49 +0200

    hide implementation (class should only reveal add and pop, internals should be hidden)

--- src/main/scala/heap/Heap.scala
+++ src/main/scala/heap/Heap.scala
@@ -8,64 +8,10 @@ class Heap[T](compareFunction: (T, T) => Int) {
     private val HIGHER = 1
     private val LOWER = -1
 
-    private val heapRepresentation = new ListBuffer[T]()
-
-    private def lastIndex = heapRepresentation.size - 1
-
-    private def valueOf(index: Int): T = heapRepresentation(index)
-
     def add(item: T): Heap[T] = {
         heapRepresentation.append(item)
         bubbleUp(lastIndex)
         this
-    }
-
-    def swap(index1: Int, index2: Int) {
-        val temp = valueOf(index1)
-        heapRepresentation.update(index1, valueOf(index2))
-        heapRepresentation.update(index2, temp)
-    }
-
-    def bubbleUp(currentIndex: Int) {
-        def getParent(i: Int) = (i - 1) / 2
-
-        if (currentIndex > 0) {
-            val parentIndex = getParent(currentIndex)
-
-            compareFunction(valueOf(currentIndex), valueOf(parentIndex)) match {
-                case LOWER =>
-                    swap(currentIndex, parentIndex)
-                    bubbleUp(parentIndex)
-                case _ =>
-            }
-        }
-    }
-
-    def bubbleDown(currentIndex: Int) {
-        getLowerChild(currentIndex) match {
-            case Some((lowerChildIndex, lowerChildValue)) =>
-                if (compareFunction(valueOf(currentIndex), lowerChildValue) == HIGHER) {
-                    swap(currentIndex, lowerChildIndex)
-                    bubbleDown(lowerChildIndex)
-                }
-            case None =>
-        }
-    }
-
-    def getLowerChild(index: Int): Option[(Int, T)] = {
-        def getChildrenIndices(parentIndex: Int): (Int, Int) = (2 * parentIndex + 1, 2 * parentIndex + 2)
-
-        val (leftChildIndex, rightChildIndex) = getChildrenIndices(index)
-
-        val areChildrenInBoundsOfHeap = (rightChildIndex <= lastIndex) && (leftChildIndex <= lastIndex)
-        if (!areChildrenInBoundsOfHeap) return None
-
-        val (leftChildValue, rightChildValue) = (heapRepresentation(leftChildIndex), heapRepresentation(rightChildIndex))
-
-        compareFunction(leftChildValue, rightChildValue) match {
-            case LOWER => Some((leftChildIndex, leftChildValue))
-            case _ => Some((rightChildIndex, rightChildValue))
-        }
     }
 
     def pop: Option[T] = heapRepresentation.size match {
@@ -83,6 +29,60 @@ class Heap[T](compareFunction: (T, T) => Int) {
             Some(firstValue)
     }
 
+    private val heapRepresentation = new ListBuffer[T]()
+
+    private def lastIndex = heapRepresentation.size - 1
+
+    private def valueOf(index: Int): T = heapRepresentation(index)
+
+
+    private def swap(index1: Int, index2: Int) {
+        val temp = valueOf(index1)
+        heapRepresentation.update(index1, valueOf(index2))
+        heapRepresentation.update(index2, temp)
+    }
+
+    private def bubbleUp(currentIndex: Int) {
+        def getParent(i: Int) = (i - 1) / 2
+
+        if (currentIndex > 0) {
+            val parentIndex = getParent(currentIndex)
+
+            compareFunction(valueOf(currentIndex), valueOf(parentIndex)) match {
+                case LOWER =>
+                    swap(currentIndex, parentIndex)
+                    bubbleUp(parentIndex)
+                case _ =>
+            }
+        }
+    }
+
+    private def bubbleDown(currentIndex: Int) {
+        getLowerChild(currentIndex) match {
+            case Some((lowerChildIndex, lowerChildValue)) =>
+                if (compareFunction(valueOf(currentIndex), lowerChildValue) == HIGHER) {
+                    swap(currentIndex, lowerChildIndex)
+                    bubbleDown(lowerChildIndex)
+                }
+            case None =>
+        }
+    }
+
+    private def getLowerChild(index: Int): Option[(Int, T)] = {
+        def getChildrenIndices(parentIndex: Int): (Int, Int) = (2 * parentIndex + 1, 2 * parentIndex + 2)
+
+        val (leftChildIndex, rightChildIndex) = getChildrenIndices(index)
+
+        val areChildrenInBoundsOfHeap = (rightChildIndex <= lastIndex) && (leftChildIndex <= lastIndex)
+        if (!areChildrenInBoundsOfHeap) return None
+
+        val (leftChildValue, rightChildValue) = (heapRepresentation(leftChildIndex), heapRepresentation(rightChildIndex))
+
+        compareFunction(leftChildValue, rightChildValue) match {
+            case LOWER => Some((leftChildIndex, leftChildValue))
+            case _ => Some((rightChildIndex, rightChildValue))
+        }
+    }
 
     override def toString = {
         s"[$heapRepresentation](${heapRepresentation.size})"
